327f8b6aeb47d755007cfbb13f17b445d6815754
Paulo Carmino <contato@paulocarmino.com>
2020-02-25 19:55:49 -0300

    Home get Pokemons using GraphQL

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
 
-import { Container } from './styles';
+import { Container, Text } from './styles';
 import GridList from '~/components/GridList';
+import { GET_ALL_POKEMONS } from '~/graphql/query';
 
 export default function Home() {
+  const { loading, error, data } = useQuery(GET_ALL_POKEMONS);
+
+  if (loading) return <Text>Loading...</Text>;
+  if (error) return <Text>Error :(</Text>;
+
+  console.tron.log(data);
+
   return (
     <Container>
       <GridList />
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -3,3 +3,5 @@ import styled from 'styled-components/native';
 export const Container = styled.SafeAreaView`
   flex: 1;
 `;
+
+export const Text = styled.Text``;
