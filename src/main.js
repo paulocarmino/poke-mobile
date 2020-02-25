0299e429334fc650ea24778b8a1ee5c4d2e2d4e9
Paulo Carmino <contato@paulocarmino.com>
2020-02-25 19:25:43 -0300

    Add and make initial configs for Apollo GraphQL

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import '~/config/ReactotronConfig';
 import React from 'react';
+import {
+  Provider as PaperProvider,
+  DefaultTheme as PaperDefaultTheme,
+} from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
-import { Provider as PaperProvider } from 'react-native-paper';
-
+import ApolloClient, { gql } from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
 import Routes from './routes';
-
-import { DefaultTheme as PaperDefaultTheme } from 'react-native-paper';
 
 const DefaultTheme = {
   ...PaperDefaultTheme,
@@ -17,12 +19,30 @@ const DefaultTheme = {
   },
 };
 
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+});
+
 export default function MainApp() {
+  client
+    .query({
+      query: gql`
+        {
+          rates(currency: "USD") {
+            currency
+          }
+        }
+      `,
+    })
+    .then(result => console.tron.log(result));
+
   return (
-    <PaperProvider theme={DefaultTheme}>
-      <NavigationContainer theme={DefaultTheme}>
-        <Routes />
-      </NavigationContainer>
-    </PaperProvider>
+    <ApolloProvider client={client}>
+      <PaperProvider theme={DefaultTheme}>
+        <NavigationContainer theme={DefaultTheme}>
+          <Routes />
+        </NavigationContainer>
+      </PaperProvider>
+    </ApolloProvider>
   );
 }
