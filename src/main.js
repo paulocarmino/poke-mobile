649c1210093007e7d2e7a20efdacc0e50edb0814
Paulo Carmino <contato@paulocarmino.com>
2020-02-25 19:07:02 -0300

    Add Reactotron

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import '~/config/ReactotronConfig';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { Provider as PaperProvider } from 'react-native-paper';
